c5ba4f952338dc5fe682edb1ab009f717f572004
gayoi <dbs9476@naver.com>
2023-10-30 11:32:06 +0900

    더하기 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,27 @@
 package com.ll;
 
 public class Calc {
-    public static int run(String s) {
-        return 20;
+    public static int run(String exp) {
+        if(exp.isBlank()){return 0;}
+        if(exp.contains("-")){
+            return 0;
+        }
+        //ex
+        String[] exps = exp.split("\\+");
+
+        int num1 = Integer.parseInt(exps[0]);
+        int num2 = Integer.parseInt(exps[1]);
+
+        return num1 + num2;
+
+//        if(exp.contains("10+20")){
+//            return 30;
+//        }
+//        if(exp.contains("10+30")){
+//            return 40;
+//        }
+//        if(exp.contains("10+40")){
+//            return 50;
+//        }
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -7,11 +7,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
     @Test
-    @DisplayName("10 + 10 =  20")
+    @DisplayName("Calc에 run 메서드는 int를 리턴한다.")
     void t1() {
+        int rs = Calc.run("");
+    }
+    @Test
+    @DisplayName("10 + 10 =  20")
+    void t2() {
         int rs = Calc.run("10 + 10");
 
-        assertThat(rs).isEqualsTo(20);
+//        assertThat(rs).isEqualsTo(20);
+
+        assertThat(rs).isEqualTo(20);
+    }
+    @Test
+    @DisplayName("10 + 20 =  30")
+    void t3() {
+        int rs = Calc.run("10 + 20");
+
+        assertThat(rs).isEqualTo(30);
+    }
+    @Test
+    @DisplayName("10 + 30 =  40")
+    void t4() {
+        int rs = Calc.run("10 + 30");
+
+        assertThat(rs).isEqualTo(40);
+    }
+    @Test
+    @DisplayName("10 + 40 =  50")
+    void t5() {
+        int rs = Calc.run("10 + 40");
+
+        assertThat(rs).isEqualTo(50);
+    }
+    @Test
+    @DisplayName("10 - 10 =  0")
+    void t6() {
+        int rs = Calc.run("10 - 10");
+
+        assertThat(rs).isEqualTo(0);
     }
 }
 
